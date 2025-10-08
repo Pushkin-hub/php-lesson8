a1fc67c534ac506f007510df1ab8533c3ef88505
Pushkin-hub <pushkinanton2002@gmail.com>
2025-10-08 21:14:06 +0300

    fddfffdddff

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+    return (
+        <div className="App">
+            <nav className="navbar navbar-light bg-light">
+                <div className="container-fluid">
+                    <span className="navbar-brand mb-0 h-1">
+                        quiz App
+                    </span>
+                    <div>
+                        <span className="me-3">TODO insert user</span>
+                        <button className="btn btn-outline-seconddary btn-sm">Выйти</button>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+}
---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  
+root: path.resolve(__dirname, 'frontend'),
+
+  server: {
+    port: 3000,
+    open: true,
+    cors: true,
+    },
+  
+  // Настройки сборки
+  build: {
+    outDir: path.resolve(__dirname, 'public/js'),
+    emptyOutDir:true,
+    sourcemap: true,
+    minify: 'esbuild'
+  },
+
+    css: {
+     preprocessorOptions: {
+        scss: {
+          silenceDeprecations: [
+            'import',
+            'mixed-decls',
+            'color-functions',
+            'global-builtin',
+          ],
+        },
+     },
+  },
+})
